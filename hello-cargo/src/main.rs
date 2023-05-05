7f9762bbdbb3895ea4e250b5bef6b83d729d377b
Eito Takase <eito01178@gmail.com>
2023-05-05 13:10:31 +0900

    scope

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     println!("Hello, world!");
-    println!("{}", five());
+    let x = five();
+    println!("Initialize x: {}", x);
+    {
+        let x = x + 5;
+        println!("Scoped x: {}", x);
+    }
+    println!("Unscoped x: {}", x);
 }
 
 fn five() -> i32 {
